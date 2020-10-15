367d76104c00282da9007fbaaaa603e2b1acbe84
Adam Rice <development@hashnotadam.com>
2020-10-16 08:25:29 +1100

    Make it possible to run only a subset of tests

--- test_refactorings.rb
+++ test_refactorings.rb
@@ -10,14 +10,31 @@ def camel_case(string)
   string
 end
 
-%w[first_set_of_refactorings].each do |directory|
+def test_directory(directory)
+  directory = directory.split("/").compact.join("/")
   Dir["./#{directory}/**/*.rb"].sort.each do |file|
-    require file
-
-    module_directories = file.split(%r{[/.]})[1..-2]
-    module_names = camel_case(module_directories.join("::"))
-    test_class = Object.const_get("#{module_names}::Tests")
-
-    test_class.call
+    test_file(file)
   end
+end
+
+def test_file(file)
+  require file
+
+  module_directories = file.split(%r{[/.]})[1..-2]
+  module_names = camel_case(module_directories.join("::"))
+  test_class = Object.const_get("#{module_names}::Tests")
+
+  test_class.new.call
+end
+
+limit_to = ARGV.shift
+
+if limit_to.nil?
+  %w[first_set_of_refactorings].each do |directory|
+    test_directory(directory)
+  end
+elsif limit_to.split("/").last.include?(".")
+  test_file(limit_to)
+else
+  test_directory(limit_to)
 end
